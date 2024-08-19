f29a75bf811f4850580c459fce78329ce087b154
joao.calza <joao.calza@memora.com.br>
2024-08-19 18:50:16 -0300

    feat: implementar "pensamento" em listar pensamentos

--- src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -7,6 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentoComponent implements OnInit {
 
+  listaPensamentos = [
+    {
+      conteudo: 'I Love Angular',
+      autoria: 'Jota',
+      modelo: 'modelo3'
+    },
+    {
+      conteudo: 'I hate JAVA',
+      autoria: 'Jota',
+      modelo: 'modelo1'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
