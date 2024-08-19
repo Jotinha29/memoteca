d22183010632db987444c1fe44ef08294d3fa97a
joao.calza <joao.calza@memora.com.br>
2024-08-19 18:50:58 -0300

    add: configura rotas da aplicação

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateIdeaComponent } from './components/pensamentos/criar-pensamentos/criar-pensamento.component';
+import { ListarPensamentoComponent } from './components/pensamentos/listar-pensamento/listar-pensamento.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : '',
+    redirectTo: 'listarPensamento',
+    pathMatch: 'full'
+  },
+  {
+    path : 'criarPensamento',
+    component: CreateIdeaComponent
+  },
+  {
+    path : 'listarPensamento',
+    component: ListarPensamentoComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
